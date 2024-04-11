d94f9f3d0de58f4b4750536eb816155c3bed0ae6
Borse Tushar Mukesh <122999869+TusharB45@users.noreply.github.com>
2024-04-11 16:11:21 +0530

    Input

--- IF.java
+++ IF.java
@@ -1,25 +1,18 @@
-public class Application {
-	public static void main(String[] args) {
-		
-		// Some useful conditions:
-		System.out.println(5 == 5);
-		System.out.println(10 != 11);
-		System.out.println(3 < 6);
-		System.out.println(10 > 100);
-		
-		// Using loops with "break": 
-		int loop = 0;
-		
-		while(true) {
-			System.out.println("Looping: " + loop);
-			
-			if(loop == 3) {
-				break;
-			}
-			
-			loop++;
-		
-			System.out.println("Running");
-		}
-	}
+public class IF {
+    public static void main(String[] args) {
+        
+        // Printing results of some conditions:
+        System.out.println("Result of 5 == 5: " + (5 == 5));
+        System.out.println("Result of 10 != 11: " + (10 != 11));
+        System.out.println("Result of 3 < 6: " + (3 < 6));
+        System.out.println("Result of 10 > 100: " + (10 > 100));
+        
+        // Using a for loop:
+        int iterationCount = 4; // Number of iterations
+        
+        for (int i = 0; i < iterationCount; i++) {
+            System.out.println("Looping: " + i);
+            System.out.println("Running");
+        }
+    }
 }
